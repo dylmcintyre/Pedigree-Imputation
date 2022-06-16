96b84a1bdff4322600c8de6f7d9a3ba180ffa6cc
dylmcintyre <dylanjmcintyre01@gmail.com>
2022-06-16 16:02:18 -0500

    added siblings

--- pedigree_imputation/pedigree_imputation.py
+++ pedigree_imputation/pedigree_imputation.py
@@ -1,6 +1,9 @@
 """Main module."""
+
+individuals= []
+
 class Individual:
-    def __init__ (self, pedigree_numb, id_numb, father,  mother, sex, age):
+    def __init__ (self, pedigree_numb, id_numb, father,  mother, sex, age= None):
         self.pedigree_numb = pedigree_numb
         self.sex = sex
         self.age = age
@@ -10,9 +13,12 @@ class Individual:
         
         @property
         def siblings(self):
-            return[]
-            
-        
+            siblings= []
+            for x in individuals:
+                if self.father == x[2] and self.mother == x[3]:
+                    siblings.append(x[2])
+                    #adds sibling ID numbers to list
+             
 class Pedigree:
     def __init__ (self, individuals):
             self.individuals = dict()
@@ -24,7 +30,6 @@ def import_ped(pedfile, mapfile):
     with open(pedfile,'r') as file:
         for line in file:
             data_list=line.split()
-            Individuals= []
-            Individuals.append(Individual(data_list[0], data_list[1], data_list[2], data_list[3], data_list[4]))
+            individuals.append(Individual(data_list[0], data_list[1], data_list[2], data_list[3], data_list[4]))
                 # not passing in age right now because I am not sure which column it is in.
             
