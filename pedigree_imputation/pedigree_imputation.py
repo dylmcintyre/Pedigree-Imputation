d8b3b577b5aff55c328317c621706fec5847ecdb
dylmcintyre <dylanjmcintyre01@gmail.com>
2022-06-21 14:54:25 -0500

    update

--- pedigree_imputation/pedigree_imputation.py
+++ pedigree_imputation/pedigree_imputation.py
@@ -4,6 +4,7 @@ individuals= []
 
 class Individual:
     def __init__ (self, pedigree_numb, id_numb, father,  mother, sex, age= None):
+        self.ped=None
         self.pedigree_numb = pedigree_numb
         self.sex = sex
         self.age = age
@@ -11,25 +12,36 @@ class Individual:
         self.mother = mother
         self.id_numb = id_numb
         
-        @property
-        def siblings(self):
-            siblings= []
-            for x in individuals:
-                if self.father == x[2] and self.mother == x[3]:
-                    siblings.append(x[2])
-                    #adds sibling ID numbers to list
+    @property
+    def siblings(self):
+        siblings= []
+        for ind in self.ped.individuals:
+            if self.id_numb != ind.id_numb and self.mother == ind.mother and self.father == ind.father:
+               siblings.append(ind)
+                #adds sibling to list
+        return(siblings)
              
 class Pedigree:
     def __init__ (self, individuals):
-            self.individuals = dict()
+        self.individuals = individuals
+        for ind in self.individuals:
+            ind.ped= self
+
+
     def print_info(self, filename):
          # print the pedigree information
          return[]
                
-def import_ped(pedfile, mapfile):
+def import_ped(pedfile, mapfile = None):
+    individuals= []
     with open(pedfile,'r') as file:
         for line in file:
             data_list=line.split()
             individuals.append(Individual(data_list[0], data_list[1], data_list[2], data_list[3], data_list[4]))
                 # not passing in age right now because I am not sure which column it is in.
-            
+    ped= Pedigree(individuals)
+
+import_ped("Fam12.ped")
+p= Individual(None, 18, 0, 0, 0)
+for x in p.siblings:
+    print(x.id_numb)
